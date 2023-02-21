9e4613163a59e56baafafa2e8c74b05f17304585
Davide Baldelli <davbaldelli@gmail.com>
2023-02-21 15:04:36 +0100

    added passives

--- Assets/Scripts/Action.cs
+++ Assets/Scripts/Action.cs
@@ -4,9 +4,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "TestAction", menuName = "Action")]
-public class Action : ScriptableObject
+public class Action : ScriptableObject, Card
 {
     [SerializeField]
     private List<ActionEffect> activeEffects = new List<ActionEffect>();
+    [SerializeField]
+    private List<ActionEffect> passiveEffects = new List<ActionEffect>();
+
+    public CardEffect[] getActiveEffects()
+    {
+        return activeEffects.ToArray();
+    }
+
+    public CardEffect[] getPassiveEffects()
+    {
+        return passiveEffects.ToArray();
+    }
 }
 
